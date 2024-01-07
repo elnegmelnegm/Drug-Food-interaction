3e01de21620806e8a9c4ed1c1f1ec478696f0dd2
elnegmelnegm <48047676+elnegmelnegm@users.noreply.github.com>
2024-01-07 18:21:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -17,6 +17,21 @@ model = load_model()
 # Input text
 user_input_text = st.text_area("Enter text:", "")
 
+# Set page configuration
+st.set_page_config(
+    page_title="ُEDA AI Chat",
+    page_icon="https://www.edaegypt.gov.eg/media/wc3lsydo/group-287.png",
+    layout="wide",
+)
+
+# Display header
+st.markdown('''
+<img src="https://www.edaegypt.gov.eg/media/wc3lsydo/group-287.png" width="250" height="100">''', unsafe_allow_html=True)
+
+# Additional information
+st.markdown('''
+Powered by Google AI <img src="https://seeklogo.com/images/G/google-ai-logo-996E85F6FD-seeklogo.com.png" width="20" height="20"> Streamlit <img src="https://global.discourse-cdn.com/business7/uploads/streamlit/original/2X/f/f0d0d26db1f2d99da8472951c60e5a1b782eb6fe.png" width="22" height="22"> Python <img src="https://i.ibb.co/wwCs096/nn-1-removebg-preview-removebg-preview.png" width="22" height="22">''', unsafe_allow_html=True)
+
 # Generate Gemini response for text
 if st.button("Generate Response"):
     try:
